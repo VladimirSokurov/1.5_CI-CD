6066721d5395496fa0cabb4ac6e029df29cdffe6
Vladimir Sokurov <sokurov.va@yandex.ru>
2023-03-14 11:09:05 +0300

    2

--- logistic/views.py
+++ logistic/views.py
@@ -2,6 +2,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Stock, Product
@@ -20,6 +21,10 @@ class StockViewSet(ModelViewSet):
     serializer_class = StockSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['products']
+
+class TestView(APIView):
+    def get(self, request):
+        return Response('hi1')
 
 
 @api_view(['GET'])
