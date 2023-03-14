38f30595af7a156496d665bfce20db230d429688
Vladimir Sokurov <sokurov.va@yandex.ru>
2023-03-14 12:02:58 +0300

    11

--- logistic/views.py
+++ logistic/views.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.views import APIView
